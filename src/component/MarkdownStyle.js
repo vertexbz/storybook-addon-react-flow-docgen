dacafbe78533a52caf1e04ce308417e6494e2307
vertex <adam.makswiej@gmail.com>
2018-09-19 19:31:00 +0200

    added missing flow type annotation

--- src/component/MarkdownStyle.js
+++ src/component/MarkdownStyle.js
@@ -7,6 +7,6 @@ const { readFileSync } = require('fs');
 const sheet = readFileSync(__dirname + '/../../node_modules/github-markdown-css/github-markdown.css').toString();//.replace(/\\n/g, ' ');
 module.exports = '\`' + sheet + '\`';`;
 
-const MarkdownStyle = () => <style dangerouslySetInnerHTML={{ __html: sheet }} />;
+const MarkdownStyle = (): React.Element<'style'> => <style dangerouslySetInnerHTML={{ __html: sheet }} />;
 
 export default MarkdownStyle;
