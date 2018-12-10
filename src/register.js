e8ba140320126478e52b6b2c92b9a9b8bd814994
vertex <adam.makswiej@gmail.com>
2018-12-10 17:34:05 +0100

    added active prop support for panel

--- src/register.js
+++ src/register.js
@@ -4,12 +4,15 @@ import addons from '@storybook/addons';
 import DocPanel from './component/DocPanel';
 import { id } from './constants';
 
+type RenderProps = {
+    active?: boolean
+};
+
 addons.register(id.Plugin, (): * => {
     addons.addPanel(id.Panel, {
         title: 'Docs',
-        // eslint-disable-next-line
-        render() {
-            return <DocPanel channel={addons.getChannel()} />;
+        render({ active }: RenderProps = { active: true }): ?React.Element<typeof DocPanel> {
+            return active ? <DocPanel channel={addons.getChannel()} /> : null;
         }
     });
 });
